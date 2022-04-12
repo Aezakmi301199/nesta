62e427ce14f13e7efd140df025d577ed0f1ff8d2
Aezakmi301199 <Konstantigov@gmail.com>
2022-04-13 02:50:16 +0500

    2

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { Appmodule } from "./app.module";
-
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
 
 
 
 async function start () {
     const port = process.env.port || 5050;
     const app = await NestFactory.create(Appmodule);
-
+    const config = new DocumentBuilder()
+    .setTitle('Урок от уби по nest')
+    .setDescription('Документация RESTAPI')
+    .setVersion('1.0')
+    .addTag('Lesha')
+    .build()
+    const document = SwaggerModule.createDocument(app,config);
+    SwaggerModule.setup('/api/docs',app,document);
     await app.listen(port, () => {
         console.log(`Server has been started on PORT : ${port}`);
     })
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,21 @@
 import { Body, Controller, Get, Injectable, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { CreateUserClass } from 'src/dto/createUser.dto';
+import { User } from './users.model';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
 
     constructor(private userService:UsersService){}
+    @ApiOperation({summary:'Создание пользователя'})
+    @ApiResponse({status:200,type:User})
     @Post()
     create(@Body() userDTO:CreateUserClass){
         return this.userService.createUser(userDTO);
     }
+    @ApiOperation({summary:'Получение всех пользователей'})
+    @ApiResponse({status:200,type:[User]})
     @Get()
     getAll(@Body() userDTO:CreateUserClass){
         return this.userService.getAllUser();
